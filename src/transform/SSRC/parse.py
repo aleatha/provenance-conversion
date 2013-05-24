91d5284806411b60f7929f1bbbc885d1136ee8aa
williamkong <wkong1@ucsc.edu>
2013-05-24 12:03:17 -0700

    williamkong <wkong1@ucsc.edu>

--- src/transform/SSRC/parse.py
+++ src/transform/SSRC/parse.py
@@ -6,8 +6,6 @@ import re
 import sys
 # used to pause the program at the end of its run
 import time
-# used to store the parameters into a python-importable data file
-import pickle
 #
 # used to iterate over the lines of all files listed in sys.argv[1:]
 # import fileinput
@@ -21,17 +19,15 @@ f.close();
 #
 # creates a new text file to store
 # the sorted component fields in
-fileNameEx = re.compile('[\S^.]*');
+fileNameEx = re.compile('[\S^.]*.');
 fileNameM = fileNameEx.match(sys.argv[1]);
-cleanFileName = fileNameM.group(0).rstrip('.txt');
+cleanFileName = fileNameM.group(0).strip('.');
 sortedFile = cleanFileName + '_sorted.txt';
-pickledFile = cleanFileName + '.pickle';
-sortedf = open(sortedFile, 'wt');
+sortedf = open(sortedFile, 'x');
 #
 # regular expressions
 restartEx = re.compile(' RESTART ');
 timestampEx = re.compile(' TIMESTAMP' );
-invalidEx = re.compile(' UID 0 PID 0 ');
 decEx = re.compile('\d*');
 spaceDecEx = re.compile(' \d*');
 uidEx = re.compile('UID \d*');
@@ -66,47 +62,21 @@ for line in data:
 	sortedf.write(':\n');
 	if restartEx.search(line):
 		#print('RESTART!\n');
-#		sortedf.write('RESTART!\n\n');
-		offset.append(None)
-		uid.append(None)
-		process.append(None)
-		abs.append(None)
-		times.append(None)
-		call.append(None)
-		rtrn.append(None)
-		reps.append(None)
-		#time.sleep(0.5);
+		sortedf.write('RESTART!\n\n');
+		time.sleep(0.5);
 	elif timestampEx.search(line):
 		#print('TIMESTAMP!\n');
-#		sortedf.write('TIMESTAMP!\n\n');
-		offset.append(None)
-		uid.append(None)
-		process.append(None)
-		abs.append(None)
-		times.append(None)
-		call.append(None)
-		rtrn.append(None)
-		reps.append(None)
-		#time.sleep(0.5);
-	elif invalidEx.search(line):
-#		sortedf.write('INVALID!\n\n')
-		offset.append(None)
-		uid.append(None)
-		process.append(None)
-		abs.append(None)
-		times.append(None)
-		call.append(None)
-		rtrn.append(None)
-		reps.append(None)
+		sortedf.write('TIMESTAMP!\n\n');
+		time.sleep(0.5);
 	else:
 		#print(line);
 		# finds and stores offset
 		offM = decEx.match(line);
 		offset.append(offM.group(0));
 		#print('Offset:', offM.group(0));
-#		sortedf.write('Offset: ');
-#		sortedf.write(offM.group(0));
-#		sortedf.write('\n');
+		sortedf.write('Offset: ');
+		sortedf.write(offM.group(0));
+		sortedf.write('\n');
 		#
 		# finds and stores UID
 		# begins by finding the string that matches
@@ -117,9 +87,9 @@ for line in data:
 		cleanUid = uidM2.group(0).strip();
 		uid.append(cleanUid);
 		#print('UID:', cleanUid);
-#		sortedf.write('UID: ');
-#		sortedf.write(cleanUid);
-#		sortedf.write('\n');
+		sortedf.write('UID: ');
+		sortedf.write(cleanUid);
+		sortedf.write('\n');
 		#
 		# finds and stores the PID
 		# begins by finding the string that matches
@@ -130,9 +100,9 @@ for line in data:
 		cleanPid = pidM2.group(0).strip();
 		pid.append(cleanPid);
 		#print('PID:', cleanPid);
-#		sortedf.write('PID: ');
-#		sortedf.write(cleanPid);
-#		sortedf.write('\n');
+		sortedf.write('PID: ');
+		sortedf.write(cleanPid);
+		sortedf.write('\n');
 		#
 		# finds and stores the process
 		# searches for "??" before looking
@@ -142,26 +112,26 @@ for line in data:
 			cleanPName = pNameM.group(0).strip();
 			process.append(cleanPName);
 			#print('Process:', cleanPName);
-#			sortedf.write('Process: ');
-#			sortedf.write(cleanPName);
-#			sortedf.write('\n');
+			sortedf.write('Process: ');
+			sortedf.write(cleanPName);
+			sortedf.write('\n');
 		else:
 			pNameM2 = pNameEx2.search(line);
 			pNameM3 = pNameEx3.match(pNameM2.group());
 			cleanPName3 = pNameM3.group(0).strip();
 			process.append(cleanPName3);
 			#print('Process:', cleanPName3);
-#			sortedf.write('Process: ');
-#			sortedf.write(cleanPName3);
-#			sortedf.write('\n');
+			sortedf.write('Process: ');
+			sortedf.write(cleanPName3);
+			sortedf.write('\n');
 		#
 		# finds and stores the A/B/S value
 		absM = absEx.search(line);
 		cleanAbs = absM.group(0).strip();
 		abs.append(cleanAbs);
-#		sortedf.write('A/B: ');
-#		sortedf.write(cleanAbs);
-#		sortedf.write('\n');
+		sortedf.write('A/B: ');
+		sortedf.write(cleanAbs);
+		sortedf.write('\n');
 		#print('A/B:', cleanAbs);
 		#
 		# finds and stores the timestamp
@@ -169,18 +139,18 @@ for line in data:
 		cleanTimes = timesM.group(0).strip();
 		times.append(cleanTimes);
 		#print("Timestamp:", cleanTimes);
-#		sortedf.write('Timestamp: ');
-#		sortedf.write(cleanTimes);
-#		sortedf.write('\n');
+		sortedf.write('Timestamp: ');
+		sortedf.write(cleanTimes);
+		sortedf.write('\n');
 		#
 		# finds and stores the system call
 		callM = callEx.search(line);
 		cleanCall = callM.group(0).strip();
 		call.append(cleanCall);
 		#print('Call:', cleanCall);
-#		sortedf.write('Call: ');
-#		sortedf.write(cleanCall);
-#		sortedf.write('\n');
+		sortedf.write('Call: ');
+		sortedf.write(cleanCall);
+		sortedf.write('\n');
 		#
 		# finds the return value for the
 		# system call found above and stores it
@@ -189,9 +159,9 @@ for line in data:
 		cleanRtrn = rtrnM2.group(0).strip();
 		rtrn.append(cleanRtrn);
 		#print("Return:", cleanRtrn);
-#		sortedf.write('Return: ');
-#		sortedf.write(cleanRtrn);
-#		sortedf.write('\n');
+		sortedf.write('Return: ');
+		sortedf.write(cleanRtrn);
+		sortedf.write('\n');
 		#
 		# if available, finds the number of repetitions
 		# of the system call found above and stores it
@@ -201,19 +171,17 @@ for line in data:
 			cleanerReps = cleanReps.strip('()');
 			reps.append(cleanReps);
 			#print("Repetitions:", cleanReps);
-#			sortedf.write('Repetitions: ');
-#			sortedf.write(cleanReps);
-#			sortedf.write('\n');
+			sortedf.write('Repetitions: ');
+			sortedf.write(cleanReps);
+			sortedf.write('\n');
 		else:
 			reps.append('1');
-#			sortedf.write('Repetitions: ');
-#			sortedf.write('1');
-#			sortedf.write('\n');
+			sortedf.write('Repetitions: ');
+			sortedf.write('1');
+			sortedf.write('\n');
 		#time.sleep(0.025);
 		#print('\n');
-#		sortedf.write('\n');
+		sortedf.write('\n');
 	i = i + 1;
-with open(pickledFile, 'wb') as f:
-	pickle.dump([offset, uid, pid, process, abs, times, call, rtrn, reps],f)
 sortedf.close();
 #time.sleep(10);
